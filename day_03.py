3e983301238839ef8f1004f7f867bd5e46019c48
Joao S O Bueno <gwidion@gmail.com>
2024-12-03 12:35:46 -0300

    Day 3 part 2

--- day_03.py
+++ day_03.py
@@ -10,3 +10,12 @@ mul = lambda x,y: x * y
 cc = re.findall(r"mul\(\d+?,\d+?\)", aa)
 
 print(sum(eval(expr) for expr in cc))
+
+# part 2:
+
+enabled = True
+do = lambda: globals().__setitem__("enabled", True) or 0
+dont = lambda: globals().__setitem__("enabled", False) or 0
+cc = re.findall(r"(mul\(\d+?,\d+?\)|don't\(\)|do\(\))", aa)
+print(sum(eval(expr.replace("'", "")) for expr in cc))
+
