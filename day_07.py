e657fcbb76dd097cd66604777fced96ef7119585
João S. O. Bueno <gwidion@gmail.com>
2024-12-08 02:21:44 -0300

    Day 7, part 1, non-naive

--- day_07.py
+++ day_07.py
@@ -1,11 +1,17 @@
-bb = aa.split("\n")
-cc = []
-for line in bb:
-    target, operands = line.split(":")
-    operands = tuple(map(int, operands.split()))
-    cc.append((int(target), operands))
+from functools import lru_cache
+import itertools
 
-# part 1:
+
+def parse(aa):
+    bb = aa.split("\n")
+    cc = []
+    for line in bb:
+        target, operands = line.split(":")
+        operands = tuple(map(int, operands.split()))
+        cc.append((int(target), operands))
+    return cc
+
+# naive part 1:
 def check_line(line):
     target = int(line[0])
     operands = line[1]
@@ -21,6 +27,34 @@ def check_line(line):
     return False
 
 
+# @lru_cache(1_000_000)
+def process_operands(target, operands):
+    if len(operands) == 1:
+        return {operands[0]}
+    results = set()
+    for partial in process_operands(target, operands[1:]):
+        if (item:=operands[0] + partial) <= target:
+            results.add(item)
+        if (item:=operands[0] * partial) <= target:
+            results.add(item)
+    return results
 
 
-%time sum(int(line[0]) for line in cc if check_line(line))
+
+def fast_check(target, operands):
+    if target in process_operands(target, operands[::-1]):
+        return target
+    return 0
+
+#aa = """..."""
+#cc = parse(aa)
+
+
+def part1(cc):
+    result = sum(fast_check(*line) for line in cc)
+    return result
+
+
+
+
+# %time sum(int(line[0]) for line in cc if check_line(line))
