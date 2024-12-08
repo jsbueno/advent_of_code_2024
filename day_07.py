0da07573716ed5f10bdac98290358298f52b666f
João S. O. Bueno <gwidion@gmail.com>
2024-12-08 02:54:24 -0300

    Day 7 part 2:
    A rewrite of the algorithm made it almost 10 times slower
    for part 1 (46ms to 338ms) - but them, part 2
    would run flawlesly on the first attempt using
    12s

--- day_07.py
+++ day_07.py
@@ -27,22 +27,46 @@ def check_line(line):
     return False
 
 
-# @lru_cache(1_000_000)
-def process_operands(target, operands):
+#@lru_cache(1_000_000)
+def process_operands(target, operands, part2=False):
     if len(operands) == 1:
         return {operands[0]}
     results = set()
+    aux = str(operands[0])
     for partial in process_operands(target, operands[1:]):
         if (item:=operands[0] + partial) <= target:
             results.add(item)
         if (item:=operands[0] * partial) <= target:
             results.add(item)
+        # if part2 and (item:=int(str(partial) +  )) <= target:
+        #    results.add(item)
+    return results
+
+def process_operands(target, operands, partial, part2=False):
+    if not operands:
+        return {partial,}
+    operations = [
+        lambda item: item + partial,
+        lambda item: item * partial
+    ]
+    if part2:
+        operations.append(lambda item: int(str(partial) + str(item)))
+
+    #if len(operands) == 1:
+    #    return set(result for operation in operations if result:=operation(operands[0]) <= target)
+
+    results = set()
+
+    for operation in operations:
+        if (next_item:= operation(operands[0])) <= target:
+            results.update(process_operands(target, operands[1:], next_item, part2=part2))
+
     return results
 
 
 
-def fast_check(target, operands):
-    if target in process_operands(target, operands[::-1]):
+def fast_check(target, operands, part2=False):
+    if target in process_operands(target, operands[1:], partial=operands[0], part2=part2):
         return target
     return 0
 
@@ -54,7 +78,14 @@ def part1(cc):
     result = sum(fast_check(*line) for line in cc)
     return result
 
+def part2(cc):
+    result = sum(fast_check(*line, part2=True) for line in cc)
+    return result
+
 
 
 
 # %time sum(int(line[0]) for line in cc if check_line(line))
+# %time part1(cc)
+
+# %time part2(cc)
