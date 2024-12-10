f1ca284430f6388f15190df4784d56d2329f7089
Joao S O Bueno <gwidion@gmail.com>
2024-12-10 15:00:56 -0300

    Day 10, part 1

--- day_10.py
+++ day_10.py
@@ -1,9 +1,12 @@
 from terminedia import V2, Directions
 
+
+DEBUG = FALSE
+
 class Map:
     def __init__(self, data):
         self.raw_data = data
-        self.data = [[int(digit) for digit in line] for line in  data.split()]
+        self.data = [[int(digit) if digit != "." else -1 for digit in line] for line in  data.split()]
         self.width = len(self.data[0])
         self.height = len(self.data)
     def __getitem__(self, pos):
@@ -19,25 +22,25 @@ class Map:
     def find_heads(self):
         return [pos for pos, height in self if height == 0]
     def walk(self, start_pos):
-        results = []
         branches = [start_pos,]
-        summits = 0
+        summits = set()
         while branches:
             new_branches = []
-            if DEBUG:
-                print(branches)
             for pos in branches:
                 current_height = self[pos]
                 for direction in Directions:
                     if self[new_pos:=(pos + direction)] != current_height + 1:
                         continue
                     if current_height == 8:
-                        summits += 1
+                        summits.add(new_pos)
                         if DEBUG:
                             print(new_pos)
                         continue
                     new_branches.append(new_pos)
             branches = new_branches
-        return summits
+        return len(summits)
     def part1(self):
         return sum(self.walk(head) for head in self.find_heads())
+
+
+# print(Map(aa)).part1())
