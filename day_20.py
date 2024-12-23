72181ab7ad263cc31ce4d67acf92704178b73212
João S. O. Bueno <gwidion@gmail.com>
2024-12-23 15:57:23 -0300

    Day 20, part 2

--- day_20.py
+++ day_20.py
@@ -9,8 +9,10 @@ TRACK = "."
 class Map:
     """Shamelessly copied from day 4 "TwoGrid" and then from Day 8 and them Day 12
     """
-    def __init__(self, data):
+    def __init__(self, data, part2=False, shortcut_dist = 20):
         self.data = data
+        self.part2 = part2
+        self.shortcut_dist = shortcut_dist
         self.reset()
 
     def reset(self):
@@ -62,15 +64,22 @@ class Map:
     def find_shortcuts(self):
         self.walk()
         heads = [self.end]
-        kernel = [d * 2 for d in Directions]
+        if self.part2:
+            s = self.shortcut_dist
+            #kernel = {V2(coord) for coord in product(range(-s, s + 1), repeat=2) if 2 <=  abs(coord[0]) + abs(coord[1])}
+            kernel = {V2(coord) for coord in product(range(-s, s + 1), repeat=2) if 2 <=  abs(coord[0]) + abs(coord[1]) <= s}
+        else:
+            kernel = [d * 2 for d in Directions]
+
         while heads:
             new_heads = []
             for head in heads:
                 current_distance = self.distances[head]
                 for component in kernel:
                     cheated_pos = head + component
-                    if (new_dist:=self.distances.get(cheated_pos, 2 ** 31))< current_distance - 2:
-                        self.shortcuts[current_distance - new_dist - 2] += 1
+                    manh = abs(component.x) + abs(component.y)
+                    if (new_dist:=self.distances.get(cheated_pos, 2 ** 31))< current_distance - manh:
+                        self.shortcuts[current_distance - new_dist - manh] += 1
                 for direction in Directions:
                     if self.distances.get(new_pos:=head + direction) == current_distance - 1:
                         new_heads.append(new_pos)
@@ -80,8 +89,15 @@ class Map:
         return self.data
 
 # aa = """..."""
+## part1:
 # mm = Map(aa)
-# part1:
 
-mm.find_shortcuts()
-print(sum(value for k, value in mm.shortcuts.items() if k >= 100))
+#mm.find_shortcuts()
+#print(sum(value for k, value in mm.shortcuts.items() if k >= 100))
+
+
+## part2:
+# mm = Map(aa, part2=True)
+
+#mm.find_shortcuts()  # takes < 15s
+#print(sum(value for k, value in mm.shortcuts.items() if k >= 100))
